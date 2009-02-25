0b18661553e1f0cbbc8bdfd98af4714db577499d
Henning Guenther <h.guenther@tu-bs.de>
2009-02-24 20:03:51 -0800

    Added JISX0208 encoding file
    darcs-hash:20090225040351-a4fee-c061eed83f2d97c98a4ed56e75f58c4bf0f9b057

--- Data/Encoding/JISX0208.hs
+++ Data/Encoding/JISX0208.hs
@@ -3,4 +3,4 @@ module Data.Encoding.JISX0208 where
 
 import Data.Encoding.Helper.Template (makeJISInstance)
 
-$( makeJISInstance "JISX0208" "jis0208.txt" )
+$( makeJISInstance "JISX0208" "JIS0208.TXT" )
