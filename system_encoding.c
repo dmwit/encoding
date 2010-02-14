f3022891df7dc2c10003bd72cf49ee195d531a07
Henning Guenther <h.guenther@tu-bs.de>
2010-02-14 15:19:39 -0800

    Remove nl_langinfo hack
    Ignore-this: 1f46fdb32bd81d98cd52744377da3dbb
    
    darcs-hash:20100214231939-a4fee-53f1a968c4e6a89f1526bbb85d9926688de4afda

--- system_encoding.c
+++ system_encoding.c
@@ -1,9 +1,6 @@
 #include "system_encoding.h"
 
 char* get_system_encoding() {
-#if !defined(mingw32_HOST_OS) && !defined(__MINGW32__)
-	return nl_langinfo(CODESET);
-#else
-	return "ASCII";
-#endif
+  setlocale(LC_ALL,"");
+  return nl_langinfo(CODESET);
 }
--- system_encoding.h
+++ system_encoding.h
@@ -1,9 +1,8 @@
 #ifndef __SYSTEM_ENCODING__
 #define __SYSTEM_ENCODING__
 
-#if !defined(mingw32_HOST_OS) && !defined(__MINGW32__)
 #include <langinfo.h>
-#endif
+#include <locale.h>
 
 char* get_system_encoding();
 
