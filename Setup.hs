c9e088e41a9b6cb77279fd7c94522b8d78b6d03d
jamesthompson <james@doctrly.com>
2018-01-26 21:05:45 -0500

    fix Setup.hs for building with GHC 8.2.2

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Distribution.Simple
-import Data.Encoding.Preprocessor.Mapping
-import Data.Encoding.Preprocessor.XMLMappingBuilder
+import           Data.Encoding.Preprocessor.Mapping
+import           Data.Encoding.Preprocessor.XMLMappingBuilder
+import           Distribution.Simple
 
-main = defaultMainWithHooks (simpleUserHooks
-                             {hookedPreProcessors = (("mapping",\_ _ -> mappingPreprocessor)
-                                                     :("mapping2",\_ _ -> mappingPreprocessor)
-                                                     :("xml",\_ _ -> xmlPreprocessor)
-                                                     :(hookedPreProcessors simpleUserHooks)
-                                                    )
-                             })
+main :: IO ()
+main = do
+  let mp  = ("mapping", \_ _ _ -> mappingPreprocessor)
+  let mp2 = ("mapping2",\_ _ _ -> mappingPreprocessor)
+  let xp  = ("xml",\_ _ _ -> xmlPreprocessor)
+  defaultMainWithHooks $ simpleUserHooks { hookedPreProcessors = mp:mp2:xp:hookedPreProcessors simpleUserHooks }
+
