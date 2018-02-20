67bb87ceff53f0178c988dd4e15eeb2daee92b84
Gregor Kleen <pngwjpgh@users.noreply.github.com>
2018-02-20 17:46:24 +0100

    Relax upper version bounds on dependencies

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,25 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
 import Distribution.Simple
 import Data.Encoding.Preprocessor.Mapping
 import Data.Encoding.Preprocessor.XMLMappingBuilder
 
+#if MIN_VERSION_Cabal(2,0,0)
 main = defaultMainWithHooks (simpleUserHooks
-                             {hookedPreProcessors = (("mapping",\_ _ -> mappingPreprocessor)
-                                                     :("mapping2",\_ _ -> mappingPreprocessor)
-                                                     :("xml",\_ _ -> xmlPreprocessor)
-                                                     :(hookedPreProcessors simpleUserHooks)
+                             {hookedPreProcessors = ( ("mapping" , \_ _ _ -> mappingPreprocessor)
+                                                    : ("mapping2", \_ _ _ -> mappingPreprocessor)
+                                                    : ("xml"     , \_ _ _ -> xmlPreprocessor)
+                                                    : (hookedPreProcessors simpleUserHooks)
                                                     )
                              })
+#else
+main = defaultMainWithHooks (simpleUserHooks
+                             {hookedPreProcessors = ( ("mapping" , \_ _ -> mappingPreprocessor)
+                                                    : ("mapping2", \_ _ -> mappingPreprocessor)
+                                                    : ("xml"     , \_ _ -> xmlPreprocessor)
+                                                    : (hookedPreProcessors simpleUserHooks)
+                                                    )
+                             })
+#endif
