8d1a612a2d000c888f48677af0c2a266e2d68ef7
Andrey Prokopenko <persiantiger@yandex.ru>
2020-04-05 13:31:55 +0300

    Relax boundaries, allow build with Cabal 2

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,13 @@
 module Main where
 
-import Distribution.Simple
-import Data.Encoding.Preprocessor.Mapping
-import Data.Encoding.Preprocessor.XMLMappingBuilder
+import           Data.Encoding.Preprocessor.Mapping
+import           Data.Encoding.Preprocessor.XMLMappingBuilder
+import           Distribution.Simple
 
 main = defaultMainWithHooks (simpleUserHooks
-                             {hookedPreProcessors = (("mapping",\_ _ -> mappingPreprocessor)
-                                                     :("mapping2",\_ _ -> mappingPreprocessor)
-                                                     :("xml",\_ _ -> xmlPreprocessor)
+                             {hookedPreProcessors = (("mapping",\_ _ _ -> mappingPreprocessor)
+                                                     :("mapping2",\_ _ _ -> mappingPreprocessor)
+                                                     :("xml",\_ _ _ -> xmlPreprocessor)
                                                      :(hookedPreProcessors simpleUserHooks)
                                                     )
                              })
