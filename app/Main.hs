698dab5f01ecb8764400738c48abdb396989558d
Arseny <a.yanchenko@enot.ai>
2025-02-04 19:06:01 +0100

    feat: CLI and files filtering by extensions

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,55 @@
 module Main (main) where
 
+import Options.Applicative
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath (takeExtension)
+
+data Input = Input 
+  { inputDir :: FilePath
+  , extensions :: [String]
+  } deriving (Show)
+
+input :: Parser Input
+input = Input 
+  <$> strArgument (metavar "INPUT_DIR" <> help "Input directory with files ...")
+  <*> some 
+      ( strOption 
+        ( short 'e' 
+        <> metavar "EXTENSIONS" 
+        <> help "Extensions ..."
+        )
+      )
+
+supportedExtensions :: [String]
+supportedExtensions = [".mapping", ".xml"]
+
+filesWithExtensions :: [FilePath] -> [String] -> [FilePath]
+filesWithExtensions files exts = filter (\file -> takeExtension file `elem` exts) files
+
+generateEncodings :: Input -> IO ()
+generateEncodings Input { inputDir = inputDir, extensions = extensions } = do
+  if not (validateExtensions extensions)
+    then error 
+      ( "Got unexpected extensions: " 
+      ++ show extensions 
+      ++ ", supported extensions: " 
+      ++ show supportedExtensions
+      )
+    else do
+      isDirExist <- doesDirectoryExist inputDir
+      if not isDirExist
+        then error ("Directory " ++ show inputDir ++ " does not exist")
+        else do 
+          files <- listDirectory inputDir
+          print $ filesWithExtensions files extensions
+  where
+    validateExtensions = all (`elem` supportedExtensions)
+
 main :: IO ()
-main = putStrLn "hello world"
+main = generateEncodings =<< execParser opts
+  where
+    opts = info (input <**> helper)
+      ( fullDesc
+      <> progDesc "Program description"
+      <> header "Header"
+      )
